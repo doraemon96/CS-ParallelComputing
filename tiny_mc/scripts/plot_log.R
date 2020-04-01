7089e65995b90f0a7f95c39c5c8e95df3ab905a7
Nicolas S <soncininicolas@gmail.com>
2020-04-01 14:02:53 -0300

    Add float optimization testing and script

---
+++ tiny_mc/scripts/plot_log.R
@@ -0,0 +1,33 @@
+# R Script to print comparison between running times
+# of different sources (compilers, versions, etc).
+
+library(ggplot2)
+
+
+SOURCE_FILE <- "out_tiny_mc.txt"
+
+
+# Set working directory to data folder
+setwd("../tiny_mc_fastlog")
+wd <- getwd()
+print(paste0("Working on directory: ",wd))
+
+# Load CSV file
+data   <- read.csv(paste0(wd,"/",SOURCE_FILE), header=FALSE)
+names(data) <- c("Libreria", "MetricaNorm")
+agData <- aggregate(data$MetricaNorm, by=list(data$Libreria),
+                FUN = function(x) c(mean = mean(x), sd = sd(x), n = length(x)))
+agData <- do.call(data.frame, agData)
+
+agData$se <- agData$x.sd / sqrt(agData$x.n)
+colnames(agData) <- c("Libreria", "mean", "sd", "n", "se")
+
+
+limits <- aes(ymax = agData$mean + agData$se,
+              ymin = agData$mean - agData$se)
+
+p <- ggplot( data = agData, aes(x = Libreria, y = mean))
+p + geom_bar( stat = "identity", position = position_dodge(0.9)) +
+    labs( x = "Funcion", y = "iteraciones/ms") +
+
+ggsave("plot.png")
---
+++ tiny_mc/tiny_mc_fastlog/tiny_mc_flog_perf.c
@@ -0,0 +1,177 @@
+/* Tiny Monte Carlo by Scott Prahl (http://omlc.ogi.edu)"
+ * 1 W Point Source Heating in Infinite Isotropic Scattering Medium
+ * http://omlc.ogi.edu/software/mc/tiny_mc.c
+ *
+ * Adaptado para CP2014, Nicolas Wolovick
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <omp.h> // handy omp_get_wtime()
+#include <string.h> // strlen
+#include <time.h> // time
+#include <math.h>
+#include <stdint.h>
+
+char t1[] = "Tiny Monte Carlo by Scott Prahl (http://omlc.ogi.edu)";
+char t2[] = "1 W Point Source Heating in Infinite Isotropic Scattering Medium";
+char t3[] = "CPU version, adapted for PEAGPGPU by Gustavo Castellano"
+			" and Nicolas Wolovick";
+
+// Performance logging parameters
+
+#ifndef DATAPOINTS
+#define DATAPOINTS 20
+#endif
+
+#ifndef WARMUP
+#define WARMUP 2
+#endif
+
+#ifndef OUTPUT_FILE
+#define OUTPUT_FILE "out_tiny_mc.txt"
+#endif
+
+#ifndef LIBRARY
+#define LIBRARY "fastlog"
+#endif
+
+
+// Default parameters
+
+#ifndef SHELLS
+#define SHELLS 101 // discretization level
+#endif
+
+#ifndef PHOTONS
+#define PHOTONS 32768 // 32K photons
+#endif
+
+#ifndef MU_A
+#define MU_A 2.0f // Absorption Coefficient in 1/cm !!non-zero!!
+#endif
+
+#ifndef MU_S
+#define MU_S 20.0f // Reduced Scattering Coefficient in 1/cm
+#endif
+
+#ifndef MICRONS_PER_SHELL
+#define MICRONS_PER_SHELL 50 // Thickness of spherical shells in microns
+#endif
+
+
+
+// Internal definitions and functions
+#define SEED (time(NULL)) // random seed
+#define MILLISEC 1E-3
+#define RNGS 131072
+
+// global state, heat and heat square in each shell
+float heat[SHELLS];
+float heat2[SHELLS];
+
+static inline float 
+fastlog2 (float x)
+{
+      union { float f; uint32_t i; } vx = { x };
+        union { uint32_t i; float f; } mx = { (vx.i & 0x007FFFFF) | 0x3f000000 };
+          float y = vx.i;
+            y *= 1.1920928955078125e-7f;
+
+              return y - 124.22551499f
+                         - 1.498030302f * mx.f 
+                                    - 1.72587999f / (0.3520887068f + mx.f);
+}
+
+static inline float
+fastlog (float x)
+{
+      return 0.69314718f * fastlog2 (x);
+}
+
+
+/***
+ * Photon
+ ***/
+
+static void photon(void) {
+	unsigned int shell;
+	float x, y, z, u, v, w, weight;
+	float xi1, xi2, t;
+	const float albedo = MU_S / (MU_S + MU_A);
+	const float shells_per_mfp = 1e4/MICRONS_PER_SHELL/(MU_A+MU_S);
+
+	x = 0.0f; y = 0.0f; z = 0.0f; /* launch */
+	u = 0.0f; v = 0.0f; w = 1.0f;
+	weight = 1.0f;
+
+	for (;;) {
+		t = -fastlog(rand()/(float)RAND_MAX); /* move */
+		x += t * u;
+		y += t * v;
+		z += t * w;
+
+		shell=sqrtf(x*x+y*y+z*z)*shells_per_mfp; /* absorb */
+		if (shell > SHELLS-1) {
+			shell = SHELLS-1;
+		}
+		heat[shell] += (1.0f-albedo)*weight;
+		heat2[shell] += (1.0f-albedo)*(1.0f-albedo)*weight*weight;  /* add up squares */
+		weight *= albedo;
+
+		/* New direction, rejection method */
+		do {
+			xi1 = 2.0f*rand()/(float)RAND_MAX - 1.0f;
+			xi2 = 2.0f*rand()/(float)RAND_MAX - 1.0f;
+			t = xi1*xi1+xi2*xi2;
+		} while (1.0f<t);
+		u = 2.0f * t - 1.0f;
+		v = xi1 * sqrtf((1.0f-u*u)/t);
+		w = xi2 * sqrtf((1.0f-u*u)/t);
+
+		if (weight < 0.001f) { /* roulette */
+			if (rand()/(float)RAND_MAX > 0.1f) break;
+			weight /= 0.1f;
+		}
+	}
+}
+
+
+/***
+ * Main matter
+ ***/
+
+int main(void)
+{
+	unsigned int i = 0;
+
+    FILE *f;
+    f = fopen(OUTPUT_FILE, "a");
+
+	// configure RNG
+	srand(SEED);
+
+    for (int dat = 0; dat < (DATAPOINTS + WARMUP); dat++){
+
+	    double start = 0.0, end = 0.0, elapsed = 0.0;
+        // start timer
+	    start = omp_get_wtime();
+	    // simulation
+	    for (i=0; i<PHOTONS; ++i) {
+		    photon();
+	    }
+        // stop timer
+	    end = omp_get_wtime();
+	    assert(start<=end);
+	    elapsed = end-start;
+
+        // write to output file
+        if (dat >= WARMUP){
+            fprintf(f, "%s, %lf\n", LIBRARY, PHOTONS/elapsed * MILLISEC);
+        }
+    }
+
+	return 0;
+}
